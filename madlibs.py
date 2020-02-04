bbec46777da8b4fe74ad5522362a30c2e50ef2ae
root <root@localhost.localdomain>
2020-02-04 07:08:10 -0500

    adding a madlibs .py

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,19 @@
 def main():
 	# write your code here
-
-
+	print("Mad libs where libs get mad.")
+	print("Start below:")
+	print("")
+	time = input("Enter a time from 1 to 12 :")
+	items = input("Enter an item :")
+	name = input("Enter a name:")
+	scream = input("Enter any sentence:")
+	action = input("Enter an action:")
+	print("")
+	print("The story goes...")
+	print("It was "+str(time)+" o'clock when I heard a knock at the door.")
+	print("I opened the door and there was a box full of"+items+" with a note saying \"From Mr."+name.capitalize()+".\"")
+	print("Just as I closed the door I heard a scream \""+scream.upper()+" .\"")
+	print("I froze in place and all I could do was"+action+".")
 
 if __name__ == '__main__':
 	main()
